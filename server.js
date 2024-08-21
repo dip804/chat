40f837c7a953a19f5361457791e64ecde3a736a6
dip804 <158097321+dip804@users.noreply.github.com>
2024-08-21 10:45:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,45 +1,65 @@
-const http=require("http");
-const express=require("express");
-const { Socket } = require("socket.io");
-const { join } = require("path");
+const http = require('http');
+const express = require('express');
+const { Server: WebSocketServer } = require('ws');
+const net = require('net');
+const path = require('path');
+const socketIo = require('socket.io');
 
-const app=express();
-
-const server=http.createServer(app);
+const app = express();
+const server = http.createServer(app);
 const port = process.env.PORT || 3000;
+const tcpPort = 4444; // Port for the TCP server
 
-app.use(express.static(__dirname+'/public'));
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/',(req,res)=>{
-    res.sendFile(__dirname+'/index.html');
+// Serve the HTML file
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-/*Socket.io setup */
-const io=require("socket.io")(server);
-var users={};
+// Set up Socket.io for WebSocket communication
+const io = socketIo(server);
+const users = {};
 
-io.on("connection",(socket)=>{
-    socket.on("new-user-joined",(username)=>{
-        users[socket.id]=username;
-        socket.broadcast.emit('user-connected',username);
-        io.emit("user-list",users);
-    });
+io.on('connection', (socket) => {
+  socket.on('new-user-joined', (username) => {
+    users[socket.id] = username;
+    socket.broadcast.emit('user-connected', username);
+    io.emit('user-list', users);
+  });
 
-    socket.on("disconnect",()=>{
-        socket.broadcast.emit('user-disconnected',user=users[socket.id]);
-        delete users[socket.id];
-        io.emit("user-list",users);
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('user-disconnected', users[socket.id]);
+    delete users[socket.id];
+    io.emit('user-list', users);
+  });
 
-    })
-    socket.on('message',(data)=>{
-        socket.broadcast.emit("message",{user:data.user,msg:data.msg});
-    })
-
-   
+  socket.on('message', (data) => {
+    socket.broadcast.emit('message', { user: data.user, msg: data.msg });
+  });
 });
 
+// Start the HTTP and WebSocket server
+server.listen(port, () => {
+  console.log(`Server started at http://localhost:${port}`);
+});
 
+// Create a TCP server to handle client connections on port 4444
+const tcpServer = net.createServer((clientSocket) => {
+  const clientId = `${clientSocket.remoteAddress}:${clientSocket.remotePort}`;
+  console.log(`TCP client connected: ${clientId}`);
 
-server.listen(port,()=>{
-console.log("Server started at"+port);
-})
+  clientSocket.on('data', (data) => {
+    console.log(`Received from ${clientId}: ${data}`);
+    // Here, you can handle incoming data from TCP clients and broadcast it or process it as needed.
+  });
+
+  clientSocket.on('end', () => {
+    console.log(`TCP client disconnected: ${clientId}`);
+  });
+});
+
+tcpServer.listen(tcpPort, '0.0.0.0', () => {
+  console.log(`TCP server listening on port ${tcpPort}`);
+});
